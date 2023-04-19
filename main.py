544848a050bf91006bdd2c7a5628a353f9c23b02
AashishH15 <aashish0415@gmail.com>
2023-04-18 22:35:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+<<<<<<< HEAD
+import csv
+
+def open_file():
+    ask = input("Enter CSV Filename: ")
+    while True:
+        try:
+            fp = open(ask + ".csv")
+            return fp
+        except:
+            print("\nError: File not found.")
+            ask  = input("Enter CSV Filename: ")
+=======
 def main():
     print("Hello World")
+    print("Hello World yay screw you H")
+    for i in range(10):
+        print("Hello Wrld")
+    #lezz go
+>>>>>>> 7077890c2f8ed58c2d82fe7d4af9dfe898a0faf0
