ed8ce397d39cfc0ed6cdc71008cacad7e065ace1
AashishH15 <aashish0415@gmail.com>
2023-07-12 22:57:21 -0400

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,46 +1,29 @@
-#imports
 import random
 import json
 import pickle
 import numpy as np
 import tensorflow as tf
 import nltk
-nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
-
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
-from collections import OrderedDict
-
-cred = credentials.Certificate('C:\\Users\\dcnat\\OneDrive\\Desktop\\Coding-Projects\\ChitChat\\chitchat-317ed-firebase-adminsdk-iig8w-dd515155ca.json')
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://chitchat-317ed-default-rtdb.firebaseio.com/'
-})
-
 lemmatizer = WordNetLemmatizer()
 intents = json.loads(open('intents.json').read())
-
 words = pickle.load(open('words.pkl', 'rb'))
 classes = pickle.load(open('classes.pkl', 'rb'))
 model = tf.keras.models.load_model('chatbot_model.h5')
-
 def clean_up_sentence(sentence):
     """
-    Cleans up sentence. Lemmatizer reduces/simplifies the word into a base form in order to better identify the meanings of words.
+    Cleans up sentence.
     :param sentence: Untokenized words.
     :return: Tokenized words.
     """
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
     return sentence_words
-
-
 def bag_of_words(sentence):
     """
-    Converts the cleaned up sentence into a comparison between it and the trained model.
+    Converts sentence into bag of words.
     :param sentence:
-    :return: 
+    :return:
     """
     sentence_words = clean_up_sentence(sentence)
     bag = [0] * len(words)
@@ -48,10 +31,7 @@ def bag_of_words(sentence):
         for i, word in enumerate(words):
             if word == w:
                 bag[i] = 1
-
     return np.array(bag)
-
-
 def predict_class(sentence):
     """
     Creates bag and probabilities to decide which is the correct class.
@@ -62,23 +42,18 @@ def predict_class(sentence):
     res = model.predict(np.array([bow]))[0]
     ERROR_THRESHOLD = 0.25
     results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
         return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
-
     return return_list
-
-    
-
 def get_response(intents_list, intents_json):
     """
+    Function
     :param intents_list:
     :param intents_json:
     :return:
     """
-
     tag = intents_list[0]['intent']
     list_of_intents = intents_json['intents']
     for i in list_of_intents:
@@ -86,36 +61,9 @@ def get_response(intents_list, intents_json):
             result = random.choice(i['responses'])
             break
     return result
-
-print("Chatbot Running. 'exit' to stop.")
-
-#while loop to keep the bot running until user wants to exit
+print("Chatbot Running")
 while True:
-    flag = False
-    try:
-        message = input("")
-        ints = predict_class(message)
-        res = get_response(ints,intents)
-        
-        if(message != "exit"):
-            print(res)
-            
-    except:
-        flag = True
-        if(message == "exit"):
-            break
-        print("Sorry I don't know~")
-    
-    #setting up a flag variable so program doesn't error if it goes through the except statement
-    if(flag == False):
-        tempOutput = get_response(ints,intents)
-        #direct upload to google reak time database
-        if(message != "exit"):
-            ref = db.reference('/User_Input')
-            ref.push().set(OrderedDict([
-                ('User_Input', message),
-                ('Model_Output', tempOutput)
-            ]))
-    
-
-
+    message = input("")
+    ints = predict_class(message)
+    res = get_response(ints, intents)
+    print(res)
