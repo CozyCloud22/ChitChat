dac294b519d2cd4c3d4e8c9150ef1ea79a272089
AashishH15 <aashish0415@gmail.com>
2023-07-12 23:54:53 -0400

    Update spell_checker.py

--- spell_checker.py
+++ spell_checker.py
@@ -1,34 +1,4 @@
-# from autocorrect import Speller
-
-# def spellchecker(input_message):
-#     spell = Speller()
-
-#     words = input_message.split()
-
-#     corrected_words = [spell(word) for word in words]
-
-#     corrected_message = ' '.join(corrected_words)
-#     return corrected_message
-
-# input_message = input("Enter a message: ")
-# corrected_message = spellchecker(input_message)
-# print("Corrected message:", corrected_message)
-
-# from gingerit.gingerit import GingerIt
-
-# def sentence_correction(input_message):
-#     parser = GingerIt()
-
-#     corrected = parser.parse(input_message)
-
-#     corrected_sentence = corrected['result']
-
-#     return corrected_sentence
-
-# input_message = input("Enter a message: ")
-# corrected_message = sentence_correction(input_message)
-# print("Corrected message:", corrected_message)
-
+# For some reason it doesnt like the run button on the right use the "Run" "Start Debugging" or "Start Without Debugging"
 from autocorrect import Speller
 from gingerit.gingerit import GingerIt
 
